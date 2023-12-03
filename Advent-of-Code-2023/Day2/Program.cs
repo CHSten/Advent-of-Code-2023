61b5039eb31be4f39ca1de5357f7efe6a56b3273
Christian Povlsen <chriskp63@gmail.com>
2023-12-03 14:27:17 +0100

    Completed Day 2 & 3 - all parts

---
+++ Advent-of-Code-2023/Day2/Program.cs
@@ -0,0 +1,150 @@
+﻿using Advent_of_Code_2023;
+
+namespace Day2
+{
+    internal class Program
+    {
+        public class Bag
+        {
+            public int RedCubes { get; set; }
+            public int GreenCubes { get; set; }
+            public int BlueCubes { get; set; }
+        }
+
+        public class Game
+        {
+            public int ID { get; set; }
+            public List<Bag> Bags { get; set;}
+        }
+
+        static void Main(string[] args)
+        {
+            string input = Input.Get();
+            Console.WriteLine("--PART 1:--");
+            int totalIDs = Part1(input);
+            Console.WriteLine($"The combined IDs from all possible games is: {totalIDs}\n");
+
+            Console.WriteLine("--PART 2:--");
+            int totalPower = Part2(input);
+            Console.WriteLine($"The total power of minimum red, green and blue from all possible games is: {totalPower}\n");
+
+        }
+
+        public static int Part1(string input)
+        {
+            List<Game> games = new();
+            int maxRedCubes = 12;
+            int maxGreenCubes = 13;
+            int maxBlueCubes = 14;
+
+            string[] lines = input.Split("\r\n");
+
+            processGames(games, lines);
+
+            int totalIDs = 0;
+            foreach (Game game in games)
+            {
+                bool addGameID = true;
+
+                foreach (Bag bag in game.Bags)
+                {
+                    if ((bag.RedCubes > maxRedCubes) ||
+                        (bag.GreenCubes > maxGreenCubes) ||
+                        (bag.BlueCubes > maxBlueCubes))
+                    {
+                        addGameID = false;
+                    }
+                }
+
+                if (addGameID)
+                {
+                    totalIDs += game.ID;
+                }
+            }
+
+            return totalIDs;
+        }
+
+        public static int Part2(string input)
+        {
+            List<Game> games = new();
+
+            string[] lines = input.Split("\r\n");
+
+            processGames(games, lines);
+
+            int totalPower = 0;
+            foreach (Game game in games)
+            {
+                int minimumRed = 0;
+                int minimumGreen = 0;
+                int minimumBlue = 0;
+
+                foreach (Bag bag in game.Bags)
+                {
+                    if (bag.RedCubes > minimumRed)
+                        minimumRed = bag.RedCubes;
+
+                    if (bag.GreenCubes > minimumGreen)
+                        minimumGreen = bag.GreenCubes;
+
+                    if (bag.BlueCubes > minimumBlue)
+                        minimumBlue = bag.BlueCubes;
+                }
+
+                totalPower += minimumRed * minimumGreen * minimumBlue;
+            }
+
+            return totalPower;
+        }
+
+        private static void processGames(List<Game> games, string[] lines)
+        {
+            foreach (string line in lines)
+            {
+                Game game = new();
+                game.Bags = new List<Bag>();
+
+                string idString = line.Substring("Game ".Length, line.IndexOf(':') - "Game ".Length);
+                game.ID = int.Parse(idString);
+
+                string recordedGames = line.Substring($"Game {idString}: ".Length);
+
+                string[] bagsString = recordedGames.Split(";");
+
+                foreach (string bagString in bagsString)
+                {
+                    Bag bag = new();
+
+                    string[] cubesString = bagString.Split(", ");
+
+                    foreach (string cubeString in cubesString)
+                    {
+                        if (cubeString.EndsWith("red"))
+                        {
+                            int substringLength = cubeString.Length - " red".Length;
+                            string redCubesString = cubeString.Substring(0, substringLength);
+                            bag.RedCubes = int.Parse(redCubesString);
+                        }
+                        else if (cubeString.EndsWith("green"))
+                        {
+                            int substringLength = cubeString.Length - " green".Length;
+                            string greenCubesString = cubeString.Substring(0, substringLength);
+                            bag.GreenCubes = int.Parse(greenCubesString);
+                        }
+                        else if (cubeString.EndsWith("blue"))
+                        {
+                            int substringLength = cubeString.Length - " blue".Length;
+                            string blueCubesString = cubeString.Substring(0, substringLength);
+                            bag.BlueCubes = int.Parse(blueCubesString);
+                        }
+                    }
+
+                    game.Bags.Add(bag);
+                }
+
+                games.Add(game);
+            }
+        }
+    }
+}
---
+++ Advent-of-Code-2023/Day3/Program.cs
@@ -0,0 +1,177 @@
+﻿using Advent_of_Code_2023;
+using System.Drawing;
+
+namespace Day3
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string input = Input.Get();
+            Console.WriteLine("--PART 1:--");
+            int sumOfPartNumbers = Part1(input);
+            Console.WriteLine($"The sum of all the part numbers is: {sumOfPartNumbers}\n");
+
+            Console.WriteLine("--PART 2:--");
+            int sumOfGearRatios = Part2(input);
+            Console.WriteLine($"The sum of all the gear ratios is: {sumOfGearRatios}\n");
+        }
+
+        public static int Part1(string input)
+        {
+            Dictionary<Point, List<Point>> symbols = new();
+            Dictionary<Point, int> numbers = new();
+            int maxXValue, maxYValue;
+
+            string[] lines = input.Split("\r\n");
+            maxYValue = lines.Length;
+            maxXValue = lines.First().Length;
+
+            processParts(symbols, numbers, lines);
+
+            Dictionary<Point, List<Point>> partNumbers = new();
+
+            assignPartNumbers(symbols, numbers, maxXValue, maxYValue, partNumbers);
+
+            int sumOfPartNumbers = 0;
+            
+            List<Point> checkedNumbers = new();
+            foreach (Point point in partNumbers.Keys)
+            {
+                if (checkedNumbers.Contains(point))
+                    continue;
+
+                sumOfPartNumbers += numbers[point];
+
+                checkedNumbers.AddRange(partNumbers[point]);
+            }
+
+            return sumOfPartNumbers;
+        }
+        public static int Part2(string input)
+        {
+            Dictionary<Point, List<Point>> symbols = new();
+            Dictionary<Point, int> numbers = new();
+            int maxXValue, maxYValue;
+
+            string[] lines = input.Split("\r\n");
+            maxYValue = lines.Length;
+            maxXValue = lines.First().Length;
+
+            processParts(symbols, numbers, lines);
+
+            Dictionary<Point, List<Point>> partNumbers = new();
+
+            assignPartNumbers(symbols, numbers, maxXValue, maxYValue, partNumbers);
+
+            List<int> gearRatios = new();
+
+            foreach (Point point in symbols.Keys)
+            {
+                List<Point> parts = symbols[point];
+
+                if (parts.Count == 2)
+                {
+                    int gear0 = numbers[parts[0]];
+                    int gear1 = numbers[parts[1]];
+                    gearRatios.Add(gear0 * gear1);
+                }
+            }
+
+            int sumOfGearRatios = 0;
+            foreach (int gearRatio in gearRatios)
+            {
+                sumOfGearRatios += gearRatio;
+            }
+
+            return sumOfGearRatios;
+        }
+
+        private static void assignPartNumbers(Dictionary<Point, List<Point>> symbols, Dictionary<Point, int> numbers, int maxXValue, int maxYValue, Dictionary<Point, List<Point>> partNumbers)
+        {
+            foreach (Point point in numbers.Keys)
+            {
+                for (int y = -1; y <= 1; y++)
+                {
+                    for (int x = -1; x <= 1; x++)
+                    {
+                        if (x == 0 && y == 0)
+                            continue;
+
+                        if (partNumbers.ContainsKey(point))
+                            continue;
+
+                        Point neighbor = new Point(point.X + x, point.Y + y);
+
+                        if (0 > neighbor.X || neighbor.X >= maxXValue)
+                            continue;
+
+                        if (0 > neighbor.Y || neighbor.Y >= maxYValue)
+                            continue;
+
+                        if (symbols.ContainsKey(neighbor))
+                        {
+                            symbols[neighbor].Add(point);
+                            List<Point> partNumber = new();
+                            Point nextPoint = point;
+                            while (numbers.ContainsKey(nextPoint))
+                            {
+                                partNumber.Add(nextPoint);
+                                nextPoint.X++;
+                            }
+
+                            foreach (Point newPoint in partNumber)
+                            {
+                                partNumbers.Add(newPoint, partNumber);
+                            }
+                        }
+
+                    }
+                }
+            }
+        }
+
+
+        private static void processParts(Dictionary<Point, List<Point>> symbols, Dictionary<Point, int> numbers, string[] lines)
+        {
+            for (int y = 0; y < lines.Length; y++)
+            {
+                string line = lines[y];
+                for (int x = 0; x < lines[y].Length; x++)
+                {
+                    char character = line[x];
+                    Point point = new Point(x, y);
+
+                    if (character is '.')
+                        continue;
+
+                    if (numbers.ContainsKey(point))
+                        continue;
+
+                    if (char.IsDigit(character))
+                    {
+                        string partString = string.Empty;
+                        int nextXIndex = x;
+                        while (nextXIndex < line.Length && char.IsDigit(line[nextXIndex]))
+                        {
+                            partString += line[nextXIndex];
+                            nextXIndex++;
+                        }
+
+                        int partValue = int.Parse(partString);
+
+                        for (int i = x; i < nextXIndex; i++)
+                        {
+                            Point newPoint = new Point(i, y);
+                            numbers.Add(newPoint, partValue);
+                        }
+                    }
+                    else
+                    {
+                        symbols.Add(point, new List<Point>());
+                    }
+                }
+            }
+        }
+    }
+}
